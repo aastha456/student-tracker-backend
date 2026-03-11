33f2a9a2d0926c478d30df6a974975109c84e91a
aastha456 <tamrakaraastha7@gmail.com>
2026-03-11 19:06:55 +0545

    Add environment configuration and mongodb connection

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,23 @@
 import express from 'express';
 import routes from './routes';
 import { errorHandler } from './middlewares/errorHandler';
+import { connectDB }  from './config/db';
 
+import dotenv from 'dotenv';
+
+dotenv.config();
 const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+connectDB();
 
 //used to parse json in requets body 
 app.use(express.json());
 
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+    console.log(`Server is running on port ${PORT}`);
 });
 
 app.use('/api', routes);
