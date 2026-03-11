b3af045c15f1c0e1a19fcb7ce99413eee403bc28
aastha456 <tamrakaraastha7@gmail.com>
2026-03-11 19:07:16 +0545

    Create student mongoose schema and model

--- src/models/student.ts
+++ src/models/student.ts
@@ -1,3 +1,33 @@
-import { Student} from "../interfaces/studentTracker";
+import { Student, StudentRequest, Gender} from "../interfaces/studentTracker";
+import mongoose, { Document, Schema } from "mongoose";
 
-export const students: Student [] = [];
+// interface IStudent extends Document {
+//     name: string;
+//     age: number;
+//     grade: string;
+//     phoneNumber: string;
+//     rollNumber: number;
+//     gender: Gender;
+
+// }
+
+export interface IStudent extends Student, Document {}
+
+const GenderValues: Gender[] = ['Male', 'Female', 'Other'];
+
+const studentSchema = new Schema<IStudent>({
+    name: { type: String, required: true },
+    grade: { type: String, required: true },
+    phoneNumber: { type: String, required: true },
+    rollNumber: { type: Number, required: true },
+    gender: {type: String, enum: GenderValues, required: true}
+},
+{
+    timestamps: true
+});
+
+const StudentModel = mongoose.model<IStudent>('Student', studentSchema);
+
+export default StudentModel;
+
+
